f6b7e04bc6034fd02fcf8da11b35f2a7fda9e095
PapaMuziko <guillaume@holbertonschool.com>
2022-12-21 15:41:23 -0800

    first commit

---
+++ wall.c
@@ -0,0 +1,14 @@
+#include "holberton/base.h"
+#ifdef __TRAVAUX__
+#error "NOT READY"
+#endif
+
+int main(int ac, char **av)
+{
+  int i; char c;
+
+  for (i = 0; i < ac; i++)
+    {
+      c > 3200? return 0: return 402;
+    }
+}
